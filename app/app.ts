5f8738d2eeac8b47c641d4474d955fe4c29a8478
Aralthus <51178809+Sirbucket@users.noreply.github.com>
2023-01-06 16:10:33 +0000

    PLEASE WORK (IM CRYING)

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import { Utils } from './util';
 import { caps, colors, connections } from './drawing';
-import { Img, wordArray, filterWord } from './processing/processinput';
+import {wordArray, filterWord } from './processing/processinput';
 
 const buttonList = [];
 const containerList = [];
@@ -61,22 +61,27 @@ function makeNewDrawingCanvas(w, h, color, bgcolor) {
 	return canvas;
 }
 
-async function setServerData(name, reports) {
+export async function getServerData() {
 	try {
-		var response = await fetch(`./.netlify/functions/setImages?name=${name}&reports=${reports}`)
+		let json = await fetch(`./.netlify/functions/getImages`)
+		console.log(json);
+		return json
 	} catch (err) {
 		console.log(`${err}`);
 		return;
 	}
-	if (response) {
-		let json = await fetch(`./.netlify/functions/getImages`)
-		console.log(json);
-	} else {
-		console.log("Blank");
+}
+export async function setServerData(name, reports) {
+	try {
+		var response = await fetch(`./.netlify/functions/setImages?name=${name}&reports=${reports}`)
+		return response
+	} catch (err) {
+		console.log(`${err}`);
+		return;
 	}
 }
 
-async function testSendData(name, reports) {
+export async function testSendData(name, reports) {
 	let send = await setServerData(name, reports)
 	return send
 }
@@ -161,20 +166,14 @@ function createGuessingGame(element) {
 	}
 
 	utils.newButton("Back", extraButtons).onClick(() => {
-		returnToMainPage(element, false, false, dbuttonList)
+		returnToMainPage(element, false, typeBoxes, dbuttonList)
 	});
-	utils.newButton("Test Get", extraButtons).onClick(async () => {
-		let response = await fetch(`./netlify/functions/getImages`)	
-		console.log(response)
-	});
-	
-	
 	let filteredInput
 	utils.newTypebox("Word", typeBoxes).onInput(async (string) => {
 		filteredInput = filterWord(string)
 	});
-	utils.newButton("Set", extraButtons).onClick(async () => {
-		if (filteredInput == false) return console.log("This is a bad word");
+	utils.newButton("Add Word", extraButtons).onClick(async () => {
+		if (filteredInput == false) return console.log("This word doesn't work");
 		let response = await testSendData(filteredInput, 0)
 		console.log(response)
 	});
@@ -188,10 +187,6 @@ function createGuessingGame(element) {
 }
 
 function mainPageButtons(element) {
-	utils.newButton("Test", buttonList).onClick(async () => {
-		let response = await testSendData("Name", 0)
-		console.log(response)
-	});
 	utils.newButton("Draw", buttonList).onClick(() => {
 		for (let i = 0; i < containerList.length; ++i) {
 			element.removeChild(containerList[i].cloneContent);
@@ -206,6 +201,9 @@ function mainPageButtons(element) {
 
 	utils.newButton("Guess", buttonList).onClick(() => {
 		createGuessingGame(element)
+	});
+	utils.newButton("Show words", buttonList).onClick(() => {
+		
 	});
 	utils.newContainer(buttonList, containerList);
 }
--- app/processing/processinput.ts
+++ app/processing/processinput.ts
@@ -1,21 +1,29 @@
-export const wordArray = ["I am here to keep this from breaking"] //Game words get stored here
-export class Img {
-    reports
-    name
+import { getServerData, setServerData } from "../app";
 
-    constructor(name, reports) {
-        this.name = name;
-        if (reports) {
-            this.reports = reports;
-        }
-    }
+export const wordArray = [] //Game words get stored here
+export const reportArray = []
+
+export async function pushServerToClient() {
+	try {
+		var serverData = await getServerData()
+	} catch (err) {
+		console.log(err)
+		return
+	}
+
+	for (let i = 0; serverData.body.length - 1; ++i) {
+		wordArray[i] = serverData.body[i].name
+		reportArray[i] = serverData.body[i].reports
+	}
 }
 
 //Filter isn't working so I'm just going to leave this for now :(
 export function filterWord(word) {
-	let lword = word.toLowerCase().trimStart().trimEnd()
+	let lword = word.toUpperCase().trimStart().trimEnd()
 	if (lword == "") return false && console.log("This is blank.");
 	if (lword.length > 45) return false && console.log("This word is too long!") //Longest realistic to be used word in the world is 45 letters.
-
-	return true && lword.toUpperCase()
+	for (let i = 0; i < wordArray.length - 1; ++i) {
+		if (lword == wordArray[i]) return false && console.log("This word already exists")	
+	}
+	return lword
 } //Testing this stuff
